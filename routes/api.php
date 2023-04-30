d7d35d3a04bfe6ef2e8eafaaa5c242cf288fd03b
Vittorio Grimaldi <sam_sepiol@Vitto.localdomain>
2023-04-30 07:06:41 +0200

    config auth

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test-1', function (Request $request) {
+    return 'Test 1';
+});
